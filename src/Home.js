417052fb7dbe9d383926e5e814b5ade53af283d4
mahitalk <mamtay0211@gmail.com>
2024-12-02 17:16:58 +0530

    Add Home Page

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <iframe
+        width="420"
+        height="345"
+        src="https://www.youtube.com/embed/s2skans2dP4"
+      ></iframe>
+    </div>
+  );
+}
+export default Home;
